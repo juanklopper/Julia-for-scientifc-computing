c63570d936220fe20c9906febad05ffa6f8e9701
juanklopper <juan.klopper@uct.ac.za>
2020-05-13 18:10:26 +0200

    Continued update to Julia for medical statistics.jl  Note don eyet.

--- Julia for medical statistics.jl
+++ Julia for medical statistics.jl
@@ -2,33 +2,108 @@
 # ============================
 
 
+# Introduction
+# ------------
+
+# This new version of JULIA FOR MEDICAL STATISTICS
+# aims to show you how to use Julia for common
+# tasks in medical statistics such
+# as data imports, data manipulation, plotting,
+# and inferential test.
+
+# I assume a basic understanding of statistical
+# analysis.
+
+# Along the way, I will showcase Julia syntax and
+# some coding paradigms.
+
+# Julia is a computer language for scientic computing.
+# It is a compiled language, which means that code
+# is compiled before execution.  This is in contrast
+# to a language such as Python, which is a language
+# where the code is interpreted as it is executed.
+# The compilation of code is one of the reasons why Julia
+# is so fast at code execution.
+
+# Everthing in Julia is a type.  Unlike C and other
+# strongly typed languages, Julia infers the type
+# as required and makes use of multiple dispatch
+# for type inference.  This does not prevent you from
+# specifying types.  Specifying type does indeed help to
+# speed up code execution even further as the compiled
+# code is optimized when compiled.
+
+# Julia is easy to learn and use.  The syntax is very
+# simuilar to Python.
+
+# Below, we start by importing some packages which we
+# will use in this file.
+
+
 # Package import
 # --------------
 
-using DataFrames
-using Distributions
-using Gadfly
-using HypothesisTests
-using StatsBase
-using Statistics
-using Random
-using CSV
-using Query
+# Hit SHIFT+ENTER (SHIFT+RETURN) at the end of a line of code
+# to execute it.  Some of the packages might need precompilation
+# before it is ready to use.
 
+using DataFrames  # A package for data manipulation
+using Distributions  # A package for using statistical distributions
+using Plots  # A package for creating graphs and plots
+plotly()  # USing Plotly as the backend
+using HypothesisTests  # A package with many statisical tests
+using StatsBase  # A packahe with useful statistical functions
+using Statistics  # built-in Julia package with common statistical tests
+using Random  # A package to create random values
+using CSV  # Package to import and export CSV files
+using Query  # A package to manipulate data
 
 # Creating a dataframe object
 # -------------------------------
 
-groups = ["Placebo", "Interventions"]  # Creating list objects
-stages = ["I", "II", "III", "IV"]  # Creating list objects
+# Julia has different collections.  Below, we use a list object.
+# List obejcts are indicated by the square bracket notation.  Elements
+# of a list are separated by commas.  Lists are mutable (the individual
+# elements can be altered at a later stage).  We can also use a mix
+# of element types such as integers, strings, and so on.  Note that
+# we are using strings elements below.  Strings are denoted by the
+# use of double quotes (single quotes denote characters).
+# The assigment operator, =, assigns what is to the right of it (a list
+# object in our two examples below) to what is to its left (in this case,
+# a computer variable name).
+# Computer variable names are chosen by us.  Keep them short-and-sweet,
+# but indicative of what content they hold.  Don't use illegal characters
+# or actual Julia keywords (functions).  Lowercase letter are preferred.
+# Use underscore to concatenate word together (called snake-case), i.e.
+# my_variable_name.
+
+# Creating list objects
+groups = ["Placebo", "Interventions"]
+stages = ["I", "II", "III", "IV"]
+
+# The seed! function in the Random package seeds the pseudorandom
+# number generator so that we get back the same "random" numbers
+# everytime the code is run.  Below, we seed it with the integer 12.
+# The semi-colon suppresses any results from being displayed.
 
 Random.seed!(12);  # For reproducible results
+
+# The DataFrame function from the DataFrames package creates a dataframe
+# object.  We have variable names and the data point values for that
+# viriable as arguments.
+
 # Creating the dataframe object with variables: ID, Group, Stage, Age,
 # Cholesterol and with 100 subjects
-df = DataFrame(ID = range(1, stop = 100), Group = StatsBase.sample(groups, 100),
-    Stage = StatsBase.sample(stages, 100), Age = rand(18:55, 100),
-    Cholesterol = round.(rand(Distributions.Normal(7,2), 100), digits = 1))
+df = DataFrames.DataFrame(
+ID = range(1, stop = 100),
+Group = StatsBase.sample(groups, 100),
+Stage = StatsBase.sample(stages, 100),
+Age = rand(18:55, 100),
+Cholesterol = round.(rand(Distributions.Normal(7,2), 100), digits = 1)
+)
 
+# We can change the type of column to categorical instead of the
+# string type that they initially hold.
 DataFrames.categorical!(df, :Group)  # Setting variables as categorical
 DataFrames.categorical!(df, :Stage)
 
@@ -36,64 +111,107 @@ DataFrames.categorical!(df, :Stage)
 # Slicing
 # -------
 
-df[1:3, :]  # Rows 1, 2, and 3, for all columns
+# Slicing is the ability to select only certain data point values
+# in a collection based on using references to the index (row and
+# column number of a value).
 
-df[1:3, :Age]  # Rows 1, 2, and 3, for only the Age column (returns a vector)
+# Rows 1, 2, and 3, for all columns
+df[1:3, :]
 
-df[1:3, [:Group, :Age]]  # Rows 1, 2, and 3, for Group and Age columns
+# Rows 1, 2, and 3, for only the Age column (returns a vector)
+df[1:3, :Age]
 
-df[[1, 3], [:Group, :Age]]  # Rows 1 and 3 for Group and Age columns
+# Rows 1, 2, and 3, for Group and Age columns
+df[1:3, [:Group, :Age]]
 
-df[!, :Age] == df[:, :Age]  # Alternative syntax
+# Rows 1 and 3 for Group and Age columns
+df[[1, 3], [:Group, :Age]]
 
-df[df.Age .> 50, :]  # Show all column but only if Age value is larger than 50
+# Alternative syntax
+df[!, :Age] == df[:, :Age]
 
-df[(df.Age .> 50) .& (df.Group .== "Placebo"), :]  # Show all columns but only if Age is larger than 50 AND Group us Placebo
+# Show all column but only if Age value is larger than 50
+df[df.Age .> 50, :]
 
-df[in.(df.Age, Ref([20, 30, 40])), :]  # Show all columns but only for selected Age values
+# Show all columns but only if Age
+# is larger than 50 AND Group us Placebo
+df[(df.Age .> 50) .& (df.Group .== "Placebo"), :]
 
-placebo = df[(df.Group .== "Placebo"), :]  # Creating a sub-dataframe object
-intervention = df[(df.Group .!= "Placebo"), :]  # Creating a sub-dataframe object
+# Show all columns but only for selected Age values
+df[in.(df.Age, Ref([20, 30, 40])), :]
+
+# We can create new dataframe object (sub-dataframes) that
+# contain only a subset of the original data
+
+# Creating a sub-dataframe object
+placebo = df[(df.Group .== "Placebo"), :]
+
+# Creating a sub-dataframe object
+intervention = df[(df.Group .!= "Placebo"), :]
 
 
 # Summary statistics
 # --------------------
 
-StatsBase.describe(df[!, :Age]) # Summary statistics of Age values
+# Between the StatsBase and the Statistics packages, we find all
+# the useful statistical tests that we use everyday.
 
-StatsBase.summarystats(df[!, :Age])  # Summary statistics of Age values
+# Summary statistics of Age values
+# The results are displayed in the REPL
+StatsBase.describe(df[!, :Age])
 
-Statistics.std(df[!, :Age])  # Standard deviation of the Age values
+# Summary statistics of Age values (printing in the IDE)
+StatsBase.summarystats(df[!, :Age])
 
-StatsBase.mean_and_std(df[!, :Age])  # Mean and standrad deviation of the Age values
+# Median age (using alternative syntax)
+Statistics.median(df[:, "Age"])
 
-Statistics.quantile(df[!, :Age], [0.25, 0.75])  # First and third quartile of the Age values
+# Standard deviation of the Age values
+Statistics.std(df[!, :Age])
 
-StatsBase.iqr(df[!, :Age])  # Interquartile range of the Age values
+# Mean and standrad deviation of the Age values
+StatsBase.mean_and_std(df[!, :Age])
 
-StatsBase.span(df[!, :Age])  # Range of the Age values
+# Variance in the age
+Statistics.var(df[!, :Age])
 
-StatsBase.describe(df[!, [:Age, :Cholesterol]], :AVE => StatsBase.mean, :STD => StatsBase.std)  # Mean and std of selected columns
+# First and third quartile of the Age values
+Statistics.quantile(df[!, :Age], [0.25, 0.75])
 
-unique(df[!, :Group])  # Sample space elements of Group variable
+# Interquartile range of the Age values
+StatsBase.iqr(df[!, :Age])
+Statistics.quantile(df[!, :Age], 0.75) - Statistics.quantile(df[!, :Age], 0.25)
 
-StatsBase.countmap(df[!, :Group])  # Frequency of sample space elements of Group variable
+# Range of the Age values
+StatsBase.span(df[!, :Age])
+
+# Mean and std of selected columns
+StatsBase.describe(df[!, [:Age, :Cholesterol]], :AVE => StatsBase.mean, :STD => StatsBase.std)
+
+# Sample space elements of Group variable
+unique(df[!, :Group])
+
+# Frequency of sample space elements of Group variable
+StatsBase.countmap(df[!, :Group])
+
+# Median age of only the subjects older than 50 in the placebo group
+Statistics.median(df[(df.Age .> 50) .& (df.Group .== "Placebo"), :][!, :Age])
 
 
 # Data visualization
 # ------------------
 
-Gadfly.set_default_plot_size(1200px, 600px)  # Set default plot size
+#Gadfly.set_default_plot_size(1200px, 600px)  # Set default plot size
+Plots.theme(:ggplot2)
 
 # Bar chart of Groups
-Gadfly.plot(
-    df,
-    x = "Group",
-    color = "Group",
-    Geom.histogram,
-    Guide.title("Frequency count of groups"),
-    Guide.ylabel("Count")
-)
+Plots.histogram(df.Age,
+bins = 10,
+fillalpha = 0.5,
+linewidth = 0,
+title = "Frequency distributionof age",
+xlabel = "Age",
+ylabel = "Frequency")
 
 # Box-and-whisker plot of Age by Group
 Gadfly.plot(
